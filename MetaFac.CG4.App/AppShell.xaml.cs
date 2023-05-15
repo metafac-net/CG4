b74126478010af672d44b876149c352716bc8590
lucian <lucian@metafac.net>
2023-05-15 13:45:36 +1000

    added skeleton MAUI app

---
+++ MetaFac.CG4.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MetaFac.CG4.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MetaFac.CG4.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MetaFac.CG4.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
