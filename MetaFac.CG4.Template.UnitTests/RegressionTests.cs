2d31758605cbd2957ec4ee77ad3d8cee73999bbd
lucian <lucian@metafac.net>
2023-06-09 14:39:40 +1000

    more regression tests

--- MetaFac.CG4.Template.UnitTests/RegressionTests.cs
+++ MetaFac.CG4.Template.UnitTests/RegressionTests.cs
@@ -17,13 +17,30 @@ namespace MetaFac.CG4.Template.UnitTests
             JsonNewtonSoft,
         }
 
-        private static readonly JsonSerializer serializer = new JsonSerializer() { Formatting = Formatting.Indented };
+        public enum TestFieldId
+        {
+            None,
+            UnaryOther,
+            UnaryMaybe,
+        }
+
+        private static readonly JsonSerializer jsonSerializer = new JsonSerializer()
+        {
+            Formatting = Formatting.Indented,
+            //NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
+
+        private static readonly MessagePackSerializerOptions messagePackOptions
+            = MessagePackSerializerOptions
+                .Standard
+                .WithCompression(MessagePackCompression.Lz4Block);
 
         private static string SerializeToJson<T>(T value)
         {
             using (var writer = new StringWriter())
             {
-                serializer.Serialize(writer, value);
+                jsonSerializer.Serialize(writer, value);
                 return writer.ToString();
             }
         }
@@ -32,7 +49,7 @@ namespace MetaFac.CG4.Template.UnitTests
         {
             using var tr = new StringReader(buffer);
             using var reader = new JsonTextReader(tr);
-            return serializer.Deserialize<T>(reader) ?? throw new JsonSerializationException();
+            return jsonSerializer.Deserialize<T>(reader) ?? throw new JsonSerializationException();
         }
 
         private static string CreateAndSerialize(WireFormat wf, T_Namespace_.Contracts.IT_EntityName_ source)
@@ -41,11 +58,9 @@ namespace MetaFac.CG4.Template.UnitTests
             {
                 case WireFormat.MessagePack:
                     {
-                        var options = MessagePackSerializerOptions.Standard
-                            .WithCompression(MessagePackCompression.Lz4Block);
                         var original = new T_Namespace_.MessagePack.T_EntityName_(source);
                         original.Freeze();
-                        byte[] buffer = MessagePackSerializer.Serialize(original, options);
+                        byte[] buffer = MessagePackSerializer.Serialize(original, messagePackOptions);
                         string text = string.Join('-', buffer.Select(b => b.ToString("X2")));
                         return text;
                     }
@@ -73,9 +88,7 @@ namespace MetaFac.CG4.Template.UnitTests
                         {
                             buffer[i] = byte.Parse(bytes[i], System.Globalization.NumberStyles.HexNumber);
                         }
-                        var options = MessagePackSerializerOptions.Standard
-                            .WithCompression(MessagePackCompression.Lz4Block);
-                        var duplicate = MessagePackSerializer.Deserialize<T_Namespace_.MessagePack.T_EntityName_>(buffer, options);
+                        var duplicate = MessagePackSerializer.Deserialize<T_Namespace_.MessagePack.T_EntityName_>(buffer, messagePackOptions);
                         return new T_Namespace_.RecordsV2.T_EntityName_(duplicate);
                     }
                 case WireFormat.JsonNewtonSoft:
@@ -88,31 +101,53 @@ namespace MetaFac.CG4.Template.UnitTests
             }
         }
 
+        private static T_Namespace_.RecordsV2.T_EntityName_ ModifyRecord(T_Namespace_.RecordsV2.T_EntityName_ original, TestFieldId fieldId)
+        {
+            switch (fieldId)
+            {
+                case TestFieldId.UnaryOther: return original with { T_UnaryOtherFieldName_ = 1L };
+                case TestFieldId.UnaryMaybe: return original with { T_UnaryMaybeFieldName_ = 2L };
+                default: return original;
+            }
+        }
+
         [Theory]
         [InlineData(WireFormat.MessagePack, "C7-1C-63-D2-00-00-00-77-6F-DC-00-74-C0-00-C0-01-00-4E-03-63-00-90-00-C0-C0-C0-C0-C0-C0-C0-C0")]
-        [InlineData(WireFormat.JsonNewtonSoft,
-            """
-            {
-              "T_UnaryModelFieldName_": null,
-              "T_ArrayModelFieldName_": null,
-              "T_IndexModelFieldName_": null,
-              "T_UnaryMaybeFieldName_": null,
-              "T_ArrayMaybeFieldName_": null,
-              "T_IndexMaybeFieldName_": null,
-              "T_UnaryOtherFieldName_": 0,
-              "T_ArrayOtherFieldName_": null,
-              "T_IndexOtherFieldName_": null,
-              "T_UnaryBufferFieldName_": null,
-              "T_ArrayBufferFieldName_": null,
-              "T_IndexBufferFieldName_": null,
-              "T_UnaryStringFieldName_": null,
-              "T_ArrayStringFieldName_": null,
-              "T_IndexStringFieldName_": null
-            }
-            """)]
+        [InlineData(WireFormat.JsonNewtonSoft, "{}")]
         public void RoundTrip_MultiFormat_Empty(WireFormat wf, string expected)
         {
             var original = new T_Namespace_.RecordsV2.T_EntityName_() { };
+            string serialized = CreateAndSerialize(wf, original);
+
+            // check serialized bytes for regression
+            serialized.Should().Be(expected);
+
+            // deserialize and compare to original
+            var received = Deserialize(wf, serialized);
+            received.Should().Be(original);
+            received.Equals(original).Should().BeTrue();
+            received.GetHashCode().Should().Be(original.GetHashCode());
+        }
+
+        [Theory]
+        [InlineData(WireFormat.MessagePack, TestFieldId.UnaryOther, "C7-1C-63-D2-00-00-00-77-6F-DC-00-74-C0-00-C0-01-00-4E-03-63-00-90-01-C0-C0-C0-C0-C0-C0-C0-C0")]
+        [InlineData(WireFormat.MessagePack, TestFieldId.UnaryMaybe, "C7-1F-63-D2-00-00-00-77-6F-DC-00-74-C0-00-C0-01-00-4E-00-63-00-C0-02-C0-C0-00-C0-C0-C0-C0-C0-C0-C0-C0")]
+        [InlineData(WireFormat.JsonNewtonSoft, TestFieldId.UnaryOther,
+            """
+            {
+              "T_UnaryOtherFieldName_": 1
+            }
+            """)]
+        [InlineData(WireFormat.JsonNewtonSoft, TestFieldId.UnaryMaybe,
+            """
+            {
+              "T_UnaryMaybeFieldName_": 2
+            }
+            """)]
+        public void RoundTrip_MultiFormat_NonEmpty(WireFormat wf, TestFieldId fieldId, string expected)
+        {
+            var original = new T_Namespace_.RecordsV2.T_EntityName_() { };
+            original = ModifyRecord(original, fieldId);
             string serialized = CreateAndSerialize(wf, original);
 
             // check serialized bytes for regression
