2ce38db7dca89bba5ad99bb29575e7683beb15ec
lucian <lucian@metafac.net>
2023-06-12 10:15:20 +1000

    more regression tests

--- MetaFac.CG4.Template.UnitTests/RegressionTests.cs
+++ MetaFac.CG4.Template.UnitTests/RegressionTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using MessagePack;
+using MetaFac.Memory;
 using Newtonsoft.Json;
 using System;
 using System.IO;
@@ -23,6 +24,8 @@ namespace MetaFac.CG4.Template.UnitTests
             UnaryOther,
             UnaryMaybe,
             UnaryModel,
+            UnaryChars,
+            UnaryBytes,
         }
 
         private static readonly JsonSerializer jsonSerializer = new JsonSerializer()
@@ -109,6 +112,8 @@ namespace MetaFac.CG4.Template.UnitTests
                 case TestFieldId.UnaryOther: return original with { T_UnaryOtherFieldName_ = 1L };
                 case TestFieldId.UnaryMaybe: return original with { T_UnaryMaybeFieldName_ = 2L };
                 case TestFieldId.UnaryModel: return original with { T_UnaryModelFieldName_ = new T_Namespace_.RecordsV2.T_ModelType_() { TestData = 123 } };
+                case TestFieldId.UnaryChars: return original with { T_UnaryStringFieldName_ = "abc" };
+                case TestFieldId.UnaryBytes: return original with { T_UnaryBufferFieldName_ = new Octets(new byte[] { 1, 2, 3 }) };
                 default: return original;
             }
         }
@@ -136,8 +141,12 @@ namespace MetaFac.CG4.Template.UnitTests
             "C7-1C-63-D2-00-00-00-77-6F-DC-00-74-C0-00-C0-01-00-4E-03-63-00-90-01-C0-C0-C0-C0-C0-C0-C0-C0")]
         [InlineData(WireFormat.MessagePack, TestFieldId.UnaryMaybe, 
             "C7-1F-63-D2-00-00-00-77-6F-DC-00-74-C0-00-C0-01-00-4E-00-63-00-C0-02-C0-C0-00-C0-C0-C0-C0-C0-C0-C0-C0")]
-        [InlineData(WireFormat.MessagePack, TestFieldId.UnaryModel, 
+        [InlineData(WireFormat.MessagePack, TestFieldId.UnaryModel,
             "C7-20-63-D2-00-00-00-79-6F-DC-00-74-C0-00-C0-01-00-4E-41-00-92-C0-7B-65-00-90-00-C0-C0-C0-C0-C0-C0-C0-C0")]
+        [InlineData(WireFormat.MessagePack, TestFieldId.UnaryChars,
+            "C7-1C-63-D2-00-00-00-7A-6F-DC-00-74-C0-00-C0-01-00-4E-03-63-00-02-07-00-60-A3-61-62-63-C0-C0")]
+        [InlineData(WireFormat.MessagePack, TestFieldId.UnaryBytes,
+            "C7-21-63-D2-00-00-00-7C-6F-DC-00-74-C0-00-C0-01-00-4E-03-63-00-E0-00-C0-C0-91-C4-03-01-02-03-C0-C0-C0-C0-C0")]
         [InlineData(WireFormat.JsonNewtonSoft, TestFieldId.UnaryOther,
             """
             {
@@ -156,6 +165,18 @@ namespace MetaFac.CG4.Template.UnitTests
               "T_UnaryModelFieldName_": {
                 "TestData": 123
               }
+            }
+            """)]
+        [InlineData(WireFormat.JsonNewtonSoft, TestFieldId.UnaryChars,
+            """
+            {
+              "T_UnaryStringFieldName_": "abc"
+            }
+            """)]
+        [InlineData(WireFormat.JsonNewtonSoft, TestFieldId.UnaryBytes,
+            """
+            {
+              "T_UnaryBufferFieldName_": "AQID"
             }
             """)]
         public void RoundTrip_MultiFormat_NonEmpty(WireFormat wf, TestFieldId fieldId, string expected)
