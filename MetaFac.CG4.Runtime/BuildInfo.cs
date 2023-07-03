d0c57c5827ca63019832943725621559d3b5f4ac
lucian <lucian@metafac.net>
2023-07-03 20:22:52 +1000

    exposed build info in CG4 runtime

---
+++ MetaFac.CG4.Runtime/BuildInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MetaFac.CG4.Runtime
+{
+    public class BuildInfo : IBuildInfo
+    {
+        private static readonly BuildInfo _instance = new BuildInfo();
+        public static IBuildInfo Instance => _instance;
+
+        string IBuildInfo.AssemblyName => ThisAssembly.AssemblyName;
+        string IBuildInfo.AssemblyConfiguration => ThisAssembly.AssemblyConfiguration;
+        string IBuildInfo.AssemblyVersion => ThisAssembly.AssemblyVersion;
+        string IBuildInfo.AssemblyFileVersion => ThisAssembly.AssemblyFileVersion;
+        string IBuildInfo.AssemblyInformationalVersion => ThisAssembly.AssemblyInformationalVersion;
+        string IBuildInfo.GitCommitId => ThisAssembly.GitCommitId;
+        DateTime IBuildInfo.GitCommitDate => ThisAssembly.GitCommitDate;
+    }
+}
---
+++ MetaFac.CG4.Runtime/IBuildInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MetaFac.CG4.Runtime
+{
+    public interface IBuildInfo
+    {
+        string AssemblyName { get; }
+        string AssemblyConfiguration { get; }
+        string AssemblyVersion { get; }
+        string AssemblyFileVersion { get; }
+        string AssemblyInformationalVersion { get; }
+        string GitCommitId { get; }
+        DateTime GitCommitDate { get; }
+    }
+}
